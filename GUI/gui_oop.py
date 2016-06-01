e7071271e5790cec14aab0d1fb827df0387b145f
sravan <gottapu@iitg.ernet.in>
2016-06-01 15:07:22 +0530

    Browsing Files

--- GUI/gui_oop.py
+++ GUI/gui_oop.py
@@ -3,7 +3,8 @@ from  tkinter import *
 from tkinter import filedialog
 import os.path
 import sys
-sys.path.insert(0,'/home/rahulranjan/TextMiner/Processor')
+from tkinter import messagebox
+sys.path.insert(0,'/home/sravan/TextMiner/Processor')
 import namedEntity
 import partofSpeech
 import sentTokenize
@@ -20,7 +21,7 @@ class gui:
   self.root = Tk()
   self.root.minsize(250, 250)
   self.root.title("TextMiner")
-  self.icon = tkinter.Image("photo", file="/home/rahulranjan/TextMiner/Data/icon.gif")
+  self.icon = tkinter.Image("photo", file="/home/sravan/TextMiner/Data/icon.png")
   self.root.tk.call('wm','iconphoto',self.root._w,self.icon)
   self.browseButton()
   self.neButton()
@@ -66,13 +67,9 @@ class gui:
 
 
  def browse(self):
-  browse = Tk() 
-  #browse.tk.call('wm','iconphoto',browse._w,self.icon)
-  self.filename = filedialog.askopenfilename(initialdir = "/home/rahulranjan/TextMiner/Data",title = "Choose your file",filetypes = (("Text files","*.txt"),))
+  self.filename = filedialog.askopenfilename(initialdir = "/home/sravan/TextMiner/Data",title = "Choose your file",filetypes = (("Text files","*.txt"),))
   self.name=(self.filename.split('/')[-1]).split('.')[0]
-  browse.destroy()
   self.docview()
-
 
  def docview(self):
   docview=Tk()
@@ -86,75 +83,95 @@ class gui:
   docview.mainloop()
 
  
- def namedEntity(self):
-  ne=Tk()
-  #ne.tk.call('wm','iconphoto',ne._w,self.icon)
-  ne.title("Named-Entity List: "+self.name)
-  neWidget=Text(ne)
-  ne_array=namedEntity.namedEntity(self.myText)
-  for i in ne_array:
-   neWidget.insert(0.0,i+"\n")		
-  neWidget.pack(fill=BOTH)
-  ne.mainloop()
+ def namedEntity(self):  #ne.tk.call('wm','iconphoto',ne._w,self.icon)
+  if hasattr(self, 'filename'):
+    ne=Tk()
+    ne.title("Named-Entity List: "+self.name)
+    neWidget=Text(ne)
+    ne_array=namedEntity.namedEntity(self.myText)
+    for i in ne_array:
+     neWidget.insert(0.0,i+"\n")    
+    neWidget.pack(fill=BOTH)
+    ne.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
  
-
  def partofSpeech(self):
-  pos=Tk()
   #pos.tk.call('wm','iconphoto',pos._w,self.icon)
-  pos.title("POS Tagged List: "+self.name)
-  posWidget=Text(pos)
-  pos_array=partofSpeech.partofSpeech(self.myText)
-  for i in pos_array:
-   posWidget.insert(0.0,i+"\n")		
-  posWidget.pack(fill=BOTH)
-  pos.mainloop()
+  if hasattr(self,'filename'):
+    pos=Tk()
+    pos.title("POS Tagged List: "+self.name)
+    posWidget=Text(pos)
+    pos_array=partofSpeech.partofSpeech(self.myText)
+    for i in pos_array:
+      posWidget.insert(0.0,i+"\n")    
+    posWidget.pack(fill=BOTH)
+    pos.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
+
  
 
  def sentTokenize(self):
-  st=Tk()
   #st.tk.call('wm','iconphoto',st._w,self.icon)
-  st.title("Tokenized Sentences List: "+self.name)
-  stWidget=Text(st)
-  st_array=sentTokenize.sentTokenize(self.myText)
-  for i in st_array:
-   stWidget.insert(0.0,i+"\n\n\n")		
-  stWidget.pack(fill=BOTH)
-  st.mainloop()
- 
-
+  if hasattr(self,'filename'):
+    st=Tk()
+    st.title("Tokenized Sentences List: "+self.name)
+    stWidget=Text(st)
+    st_array=sentTokenize.sentTokenize(self.myText)
+    for i in st_array:
+      stWidget.insert(0.0,i+"\n\n\n")   
+      stWidget.pack(fill=BOTH)
+      st.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
  def wordTokenize(self):
-  wt=Tk()
   #wt.tk.call('wm','iconphoto',wt._w,self.icon)
-  wt.title("Tokenized Words List: "+self.name)
-  wtWidget=Text(wt)
-  wt_array=wordTokenize.wordTokenize(self.myText)
-  for i in wt_array:
-   wtWidget.insert(0.0,i+"\n")		
-  wtWidget.pack(fill=BOTH)
-  wt.mainloop()
+  if hasattr(self,'filename'):
+    wt=Tk()
+    wt.title("Tokenized Words List: "+self.name)
+    wtWidget=Text(wt)
+    wt_array=wordTokenize.wordTokenize(self.myText)
+    for i in wt_array:
+      wtWidget.insert(0.0,i+"\n")   
+      wtWidget.pack(fill=BOTH)
+      wt.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
+
 
  def manual(self):
-  self.manual=Tk()
-  #docview.tk.call('wm','iconphoto',docview._w,self.icon)
-  self.manual.title("Manually Annotate: "+self.name)
-  self.manualWidget=Text(self.manual)
-  self.manualWidget.insert(0.0,self.myText)
-  self.manualWidget.pack(fill=BOTH)
-  label_1=Label(self.manual,text="Word")
-  label_2=Label(self.manual,text="Category")
-  self.entry_1=Entry(self.manual)
-  self.entry_2=Entry(self.manual)
-  label_1.pack()
-  label_2.pack()
-  self.entry_1.pack()
-  self.entry_2.pack()
-  self.manual_array=set()
-  submit=Button(self.manual,text="Submit",bg="red",command=self.submit)
-  submit.pack()
-  done=Button(self.manual,text="Done",bg="red",command=self.done)
-  done.pack()
+  if hasattr(self,'filename'):
 
-  self.manual.mainloop()
+    self.manual=Tk()
+    #docview.tk.call('wm','iconphoto',docview._w,self.icon)
+    self.manual.title("Manually Annotate: "+self.name)
+    self.manualWidget=Text(self.manual)
+    self.manualWidget.insert(0.0,self.myText)
+    self.manualWidget.pack(fill=BOTH)
+    label_1=Label(self.manual,text="Word")
+    label_2=Label(self.manual,text="Category")
+    self.entry_1=Entry(self.manual)
+    self.entry_2=Entry(self.manual)
+    label_1.pack()
+    label_2.pack()
+    self.entry_1.pack()
+    self.entry_2.pack()
+    self.manual_array=set()
+    submit=Button(self.manual,text="Submit",bg="red",command=self.submit)
+    submit.pack()
+    done=Button(self.manual,text="Done",bg="red",command=self.done)
+    done.pack()
+
+    self.manual.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
+
 
  def submit(self):
   self.manual_array.add(self.entry_1.get()+"~"+self.entry_2.get())
@@ -167,14 +184,20 @@ class gui:
   self.manual.destroy()
     
  def getManual(self):
-  manualAnnot=Tk()
-  manualAnnot.title("Manual Annotate: "+self.name)
-  manualFile=open(self.name + "_manualAnnot.txt","r+")
-  manualText=manualFile.read()
-  manualAnnotWidget=Text(manualAnnot)
-  manualAnnotWidget.insert(0.0,manualText)
-  manualAnnotWidget.pack(fill=BOTH)
-  manualAnnot.mainloop()
+  if hasattr(self,'filename'):
+
+    manualAnnot=Tk()
+    manualAnnot.title("Manual Annotate: "+self.name)
+    manualFile=open(self.name + "_manualAnnot.txt","r+")
+    manualText=manualFile.read()
+    manualAnnotWidget=Text(manualAnnot)
+    manualAnnotWidget.insert(0.0,manualText)
+    manualAnnotWidget.pack(fill=BOTH)
+    manualAnnot.mainloop()
+  else:
+    content = "Please Select a File"
+    messagebox.showinfo("Error! Oops",content)
+
   
 
 
